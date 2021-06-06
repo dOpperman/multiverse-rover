382d0cc6669ecf3952984dd8480721743c665f84
Darien Opperman <daz.opperman+interview@gmail.com>
2021-06-07 00:00:34 +0100

    Add code to determine input first line

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,4 +1,18 @@
-import { parseLine } from './index';
+import { parseLine, isInitialLine } from './index';
+
+describe('Line Parsing', () => {
+    test('initial line detected as true when takes the form digit*SPACE*digit', () => {
+        const input = '4 8';
+
+        expect(isInitialLine(input)).toEqual(true);
+    });
+
+    test('false returned when input contains non-numbers', () => {
+        const input = 'a l';
+
+        expect(isInitialLine(input)).toEqual(false);
+    });
+});
 
 test('example test', () => {
     const output = parseLine('four');
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
+export const isInitialLine = (line: string): boolean => {
+    const values = line.split(' ');
+
+    const filteredValues = values.filter(
+        (value) => value.match(/^[0-9]/g) !== null,
+    );
+
+    return filteredValues.length == 2;
+};
+
 export const parseLine = (line: string) => {
     return line.length;
 };
